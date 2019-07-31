efe15bfe91c705b024667a49c8e192c45e994a95
gexiao <345249961@qq.com>
2019-07-31 16:33:08 +0800

    4-9 done

---
+++ run.cmd
@@ -0,0 +1,9 @@
+@echo on
+
+cd /d D:\gexiao\github\dive-in-spring-boot\spring-webmvc
+
+call mvn clean package -Dmaven.test.skip=true
+
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar target/spring-webmvc-0.0.1-SNAPSHOT-war-exec.jar
+
+
